2363eb5e2b5c9fe66945d62fef652d3e209e0e84
Tomas Herrera <tomasherrera91@hotmail.com>
2012-11-14 16:42:30 -0500

    Created a rails application and added statuses

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Koombeabook::Application.config.secret_token = 'e966659923c3ea2c9ebc554fdc074241d76dc68df3e48f62a2a6ca9bc54b62d27c6168da18eae66985308a5fd85e552c17828217a67b06f93dbff451792c0a3d'
