644098e7b7730562caa54552cb6b0a1adef89143
Tomas Herrera <tomasherrera91@hotmail.com>
2012-11-16 16:43:48 -0500

    Installed devise, added user and devise views

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable,
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :first_name, :last_name, :profile_name,:email, :password, :password_confirmation, :remember_me
+  # attr_accessible :title, :body
+  validates_presence_of :first_name, :message => "You must type in your first name in order to continue with the registration process"
+  def full_name
+  	return 
+  end
+end
