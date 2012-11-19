d66ed1fd899b9d1594593113e33e90d9a59d450e
Tomas Herrera <tomasherrera91@hotmail.com>
2012-11-19 18:23:38 -0500

    Hoy

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,7 +17,11 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :profile_name,:email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
   validates_presence_of :first_name, :message => "You must type in your first name in order to continue with the registration process"
+  validates_presence_of :last_name, :message => "You must type in your last name in order to continue with the registration process"
+  validates_presence_of :profile_name, :message => "You must type in a valid profile name in order to continue with the registration process"
+  validates_uniqueness_of :profile_name
+  has_many :statuses
   def full_name
-  	return 
+    first_name + " " + last_name
   end
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,7 +1,29 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'a user should enter a first name' do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:first_name].empty?  	
+  end
+  test 'a user should enter a last name' do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:last_name].empty?  	
+  end
+  test 'a user should enter a profile name' do 
+  	user = User.new
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?  	
+  end
+  test 'a user should enter a unique profile name' do 
+  	user = User.new
+  	user.profile_name="tomasherrera91"
+  	user.email="tomasherrera91@hotmail.com"
+  	user.first_name="Tomas"
+  	user.last_name="Herrera"
+  	user.encrypted_password="password"
+  	assert !user.save
+  	assert !user.errors[:profile_name].empty?  	
+  end
 end
